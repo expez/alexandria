f819a42f77a10621bda246745011d6926ea2c25d
Lars Andersen <lars.andersen@gjensidige.no>
2015-06-22 14:03:35 +0200

    Get rid of unused pages

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -9,8 +9,6 @@ var RouteHandler = Router.RouteHandler;
 
 var routes = (
   <Route name="app" path="/" handler={require("./components/Application")}>
-    <Route name="index" transitionName="yolo" handler={require("./components/pages/Index")} addHandlerKey={true}/>
-    <Route name="about" handler={require("./components/pages/About")} addHandlerKey={true}/>
     </Route>
 );
 
